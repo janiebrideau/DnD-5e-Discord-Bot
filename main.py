820a45be889bc24a5e7f50e5c9d9799981318b6f
Janie B <janiebrideau@hotmail.com>
2022-12-12 01:02:08 +0000

    Initial Commit. Very basic bot for the time being.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 import discord
 
 intents = discord.Intents().all()
-client = discord.Client(intents=intents);
+client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -16,5 +18,5 @@ async def on_message(message):
   if message.content.startswith('!Hello'):
     await message.channel.send('Beep Boop!')
 
-client.run(os.environ['TOKEN'])
 
+client.run(os.environ['TOKEN'])
