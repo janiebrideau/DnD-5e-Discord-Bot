c30a437a3fe8bc82a99ee33066adbabde5d10b81
Janie B <janiebrideau@hotmail.com>
2022-12-14 00:32:27 +0000

    Getting name & spell description from API

--- api_requests.py
+++ api_requests.py
@@ -3,7 +3,10 @@ import json
 
 #We are using https://www.dnd5eapi.co/ API
 #API Request for Spells - hard coded for now just to test
-def spells():
-  api_response = requests.get("https://www.dnd5eapi.co/api/spells/acid-arrow")
-  json_data = json.loads(api_response.text)
-  return(json_data)
+def spells(name):
+	api_response = requests.get("https://www.dnd5eapi.co/api/spells/"+name)
+	json_data = json.loads(api_response.text)
+	description = json.dumps(json_data['desc']).strip('[]').strip('""')
+	name = json.dumps(json_data['name']).strip('""')
+	spell_info = name + ": "+description
+	return(spell_info)
--- bot.py
+++ bot.py
@@ -1,29 +1,37 @@
 import os
 import discord
-import requests
-import json
-from api_requests import *
+from api_requests import spells
 
-intents = discord.Intents().all()
-client = discord.Client(intents=intents)
+#You will need to inlude your own discord bot's token as an env variable
+#You can do this under "Secrets" in replit
+bot_token = os.environ['TOKEN']
+
+#All intents enabled for now
+client = discord.Client(intents=discord.Intents().all())
 
 
+#When the bot first starts, it displays in the console that you are logged in as (bot user)
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
 
-
+#This is to avoid the bot answering to itself
 @client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
 
-#When user types "!", the bot types the API response in the chat
+	#When user types "!", the bot types the API response in the chat
 	if message.content.startswith('!'):
-		desc = spells()
-		await message.channel.send(desc)
+		spell_name = " Acid Arrow "
+		spell_name = spell_name.strip().lower().replace(" ", "-")
+		spell_info = spells(spell_name)
+		await message.channel.send(spell_info)
+	#TO DO: read the user's input and give the spell info accordingly.
 
-client.run(os.environ['TOKEN'])
+
+client.run(bot_token)
 
 #todo: dynamic spell look up by the user
 #todo: add more look ups: feats, traits, skills, monsters
+#todo: Bot sends message to current server members or new members in a PM (only once!) explaining what it does and which commands it responds to.
